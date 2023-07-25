e1ca9e4ae5104ae56abda5da595b255cf2c1bd0f
xKiura <63990468+xKiura@users.noreply.github.com>
2023-07-25 23:31:40 +0300

    added files

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
